8edbe27e51cbfb1dae19c61e51f51c4ce524ac6f
LennoNardo <nardolenno@gmail.com>
2025-05-26 10:47:01 +0700

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,11 @@ function App() {
 
   return (
     <>
-      <h1>Update 1</h1>
-      <h1>Update 2</h1>
-      <h1>Update 3</h1>
-      <h1>Update 6</h1>
+      
     </>
   )
 }
 
 export default App
 
-function Test() {
-  return (
-    <>
-      <h1>Test</h1>
-    </>
-  )
-}
 
-function Test2() {
-  return (
-    <>
-      <h1>Tes2t</h1>
-    </>
-  )
-}
-
-function Test3() {
-  return (
-    <>
-      <h1>Tes3</h1>
-    </>
-  )
-}
